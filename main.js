de9134b81b05335188198f569b09b89e2f98a9fa
Jeizer-Valarezo <nakellava@hotmail.com>
2023-12-15 21:13:18 +0100

    Navbar

---
+++ main.js
@@ -0,0 +1,18 @@
+/* NAV INICIO */
+let listElements = document.querySelectorAll('.listN__button--click');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', () => {
+
+        listElement.classList.toggle('arrow');
+
+        let height = 0;
+        let menu = listElement.nextElementSibling;
+        if(menu.clientHeight == "0"){
+            height=menu.scrollHeight;
+        }
+
+        menu.style.height = `${height}px`
+    })
+})
+/* NAV FIN */
